90a17a19a5f9b28cfa4a3571ad9cd5f3d2bedf92
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2020-09-28 14:33:01 +0100

    Adding Server class

--- src/Options/Server.php
+++ src/Options/Server.php
@@ -1,119 +1,122 @@
 <?php
-    /**
-     * Ce fichier est une partie du Framework Ekolo
-     * (c) Don de Dieu BOLENGE <dondedieubolenge@gmail.com>
-     */
-    namespace Ekolo\Component\Http\Options;
-    
-    use Ekolo\Component\EkoMagic\ParameterBag;
 
-    /**
-     * Controlle les paramètres de $_SERVER et d'autres fonctionnalités liées au serveur
-     */
-    class Server extends ParameterBag {
+/**
+ * This file is a part of the Ekolo Builder
+ * @author Don de Dieu BOLENGE <dondedieubolenge@gmail.com>
+ */
 
-        public function __construct(array $vars = [])
-        {
-            $vars = !empty($vars) ? $vars : $_SERVER;
-            parent::__construct($vars);
-        }
+namespace Ekolo\Http\Options;
 
-        /**
-         * Permet de modifier la valeur de SERVER
-         * @param string $key La clé
-         * @param mixed $value la valeur
-         * @return void
-         */
-        public function set($key, $value)
-        {
-            parent::set($key, $value);
-            $_SERVER[$key] = $value;
-        }
+use Ekolo\EkoMagic\ParameterBag;
 
-        /**
-         * Permet d'ajouter un nouveau tableau de SERVER
-         * @param array $parameters Les paramètres à ajouter
-         * @return void
-         */
-        public function add(array $parameters = [])
-        {
-            parent::add($parameters);
-            $_SERVER = array_merge($_SERVER, $parameters);
-        }
+/**
+ * Controls the parameters of $ _SERVER (The data coming from a form by the POST method)
+ */
+class Bodies extends ParameterBag
+{
 
-        /**
-         * Remplace les variables SERVER
-         * @param array $parameters
-         * @return void
-         */
-        public function replace(array $parameters = [])
-        {
-            parent::replace($parameters);
-            $_SERVER = $parameters;
-        }
-
-        /**
-         * Supprime la variable
-         * @param string $key La clé de la variable à supprimer
-         * @return void
-         */
-        public function remove($key)
-        {
-            parent::remove($key);
-            unset($_SERVER[$key]);
-        }
-
-        /**
-         * Retourne la variable dont la clé passée en paramètre
-         * @param string $key La clé de la variable à retoruner
-         * @return void
-         */
-        public function get($key, $default = null)
-        {
-            parent::get($key, $default);
-
-            return $this->has($key) ? $_SERVER[$key] : null;
-        }
-
-        /**
-         * Retourne les variables
-         * @return array
-         */
-        public function all()
-        {
-            parent::all();
-
-            return $_SERVER;
-        }
-
-        /**
-         * Renvoi les clés des paramètres
-         * @return array
-         */
-        public function keys()
-        {
-            parent::keys();
-            return array_keys($_SERVER);
-        }
-
-        /**
-         * Vérifie si le parameter existe
-         * @param string $key
-         * @return bool
-         */
-        public function has($key)
-        {
-            parent::has($key);
-            return array_key_exists($key, $_SERVER);
-        }
-
-        /**
-         * Retourne Le nombre de parameters
-         * @return int
-         */
-        public function count()
-        {
-            parent::count();
-            return \count($_SERVER);
-        }
+    public function __construct(array $vars = [])
+    {
+        $vars = !empty($vars) ? $vars : $_SERVER;
+        parent::__construct($vars);
     }
+
+    /**
+     * Allows you to modify the value of POST element
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function set($key, $value)
+    {
+        parent::set($key, $value);
+        $_SERVER[$key] = $value;
+    }
+
+    /**
+     * Allows you to modify the value of POST...
+     * @param array $parameters
+     * @return void
+     */
+    public function add(array $parameters = [])
+    {
+        parent::add($parameters);
+        $_SERVER = array_merge($_SERVER, $parameters);
+    }
+
+    /**
+     * Replaces all variables
+     * @param array $parameters
+     * @return void
+     */
+    public function replace(array $parameters = [])
+    {
+        parent::replace($parameters);
+        $_SERVER = $parameters;
+    }
+
+    /**
+     * Remove a variable
+     * @param string $key
+     * @return void
+     */
+    public function remove($key)
+    {
+        parent::remove($key);
+        unset($_SERVER[$key]);
+    }
+
+    /**
+     * Returns the variable whose key passed in parameter
+     * @param string $key
+     * @return void
+     */
+    public function get($key, $default = null)
+    {
+        parent::get($key, $default);
+
+        return $this->has($key) ? $_SERVER[$key] : null;
+    }
+
+    /**
+     * Return all variables
+     * @return array
+     */
+    public function all()
+    {
+        parent::all();
+
+        return $_SERVER;
+    }
+
+    /**
+     * Return all parameters keys
+     * @return array
+     */
+    public function keys()
+    {
+        parent::keys();
+        return array_keys($_SERVER);
+    }
+
+    /**
+     * Check if the parameter exists
+     * @param string $key
+     * @return bool
+     */
+    public function has($key)
+    {
+        parent::has($key);
+        return array_key_exists($key, $_SERVER);
+    }
+
+    /**
+     * Return number of parameters
+     * @return int
+     */
+    public function count()
+    {
+        parent::count();
+        return \count($_SERVER);
+    }
+}
